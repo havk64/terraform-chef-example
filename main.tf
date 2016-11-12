9da7caff9e433377a65c8906ff3c475da58cdf14
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-11-12 00:47:43 -0200

    Add route table and association as comments

--- main.tf
+++ main.tf
@@ -77,3 +77,18 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+/*
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = "${aws_vpc.default.id}"
+  route_table_id = "${aws_route_table.r.id}"
+}
+*/
